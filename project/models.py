68243a4ad3fdaa1898cb712ddac9a82b2291e90c
Anna Semrau <ann.semrau2000@gmail.com>
2022-01-18 23:30:28 +0100

    Database scheme creation

--- project/models.py
+++ project/models.py
@@ -4,6 +4,20 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # TODO store hashes and salts of passwords instead
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    is_public = db.Column(db.Boolean)
+    is_encrypted = db.Column(db.Boolean)
+    encryption_key_hash = db.Column(db.String(100))
+    text = db.Column(db.String(1000))
+
+
+class Share(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    note_id = db.Column(db.Integer)
