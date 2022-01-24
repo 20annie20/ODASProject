b8f4ae4a5c0024e0ad3f00125b030969febb38ad
Anna Semrau <ann.semrau2000@gmail.com>
2022-01-24 03:47:13 +0100

    setup

--- project/__init__.py
+++ project/__init__.py
@@ -12,18 +12,20 @@ TESTING = True
 DEBUG = True
 FLASK_ENV = 'development'
 
-
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    # this would normally be stored on a server and read by a config class
     os.environ['DEBUG'] = "1"
-    os.environ['SQLALCHEMY_DATABASE_URI']="sqlite:///db.sqlite"
+    os.environ['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
     os.environ['SESSION_COOKIE_NAME'] = "ciasteczko"
     os.environ['SECRET_KEY'] = "Ochrona2022"
+    ###
     app.config.from_object('config.Config')
     db.init_app(app)
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -34,7 +36,6 @@ def create_app():
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
-
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
@@ -44,4 +45,3 @@ def create_app():
 
     app.register_blueprint(main_blueprint)
     return app
-
---
+++ readme.md
@@ -0,0 +1,6 @@
+1. Projekt był tworzony na Windowsie, lista kroków do uruchomienia w tym OS:
+2. set FLASK_APP=project
+3. venv\Scripts\activate
+4. pip install -r requirements.txt
+5. python init_database.py
+6. flask run
