da4d2245715ce61c0f5bbcb544d56b5c3adc4c16
Anna Semrau <ann.semrau2000@gmail.com>
2022-01-26 21:22:15 +0100

    Changing the debug config setup to the production setup

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
     SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     PERMANENT_SESSION_LIFETIME = 600
-    FLASK_ENV = 'development'
-    DEBUG = True
-    TESTING = True
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
--- project/__init__.py
+++ project/__init__.py
@@ -18,7 +18,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     # this would normally be stored on a server and read by a config class
-    os.environ['DEBUG'] = "1"
+    os.environ['DEBUG'] = "0"
     os.environ['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
     os.environ['SESSION_COOKIE_NAME'] = "ciasteczko"
     os.environ['SECRET_KEY'] = "Ochrona2022"
@@ -33,15 +33,16 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    with app.app_context():
+        # blueprint for auth routes in our app
+        from .auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+        # blueprint for non-auth parts of app
+        from .main import main as main_blueprint
 
-    app.register_blueprint(main_blueprint)
+        app.register_blueprint(main_blueprint)
+
     return app
